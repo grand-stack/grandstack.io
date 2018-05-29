9a66008015b4340f801482dceb1ae3bf91832a75
William Lyon <lyonwj@gmail.com>
2018-05-29 16:08:19 -0600

    Fix broken ref

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -47,7 +47,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     
-    {doc: 'getting-started', label: 'Docs'},
+    {doc: 'links', label: 'Docs'},
     //{doc: 'doc4', label: 'API'},
     {href: 'https://blog.grandstack.io', label: 'Blog'},
     //{blog: true, label: 'Blog'},
