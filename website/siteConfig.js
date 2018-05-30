c04e9740018bbed6ea2fbf0d9f9901cf19c62db8
johnymontana <lyonwj@gmail.com>
2018-05-30 16:52:53 -0600

    add graphql syntax highlighting

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -87,8 +87,56 @@ const siteConfig = {
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
     theme: "darcula",
-    // from https://github.com/isagalaev/highlight.js/pull/1684/files
     hljs: function(hljs) {
+
+      // from https://github.com/isagalaev/highlight.js/pull/1543
+      hljs.registerLanguage("graphql", function(hljs) {
+        return {
+          aliases: ["gql"],
+          keywords: {
+            keyword:
+              "query mutation subscription|10 type interface union scalar fragment|10 enum on ...",
+            literal: "true false null"
+          },
+          contains: [
+            hljs.HASH_COMMENT_MODE,
+            hljs.QUOTE_STRING_MODE,
+            hljs.NUMBER_MODE,
+            {
+              className: "type",
+              begin: "[^\\w][A-Z][a-z]",
+              end: "\\W",
+              excludeEnd: true
+            },
+            {
+              className: "literal",
+              begin: "[^\\w][A-Z][A-Z]",
+              end: "\\W",
+              excludeEnd: true
+            },
+            {
+              className: "variable",
+              begin: "\\$",
+              end: "\\W",
+              excludeEnd: true
+            },
+            {
+              className: "keyword",
+              begin: "[.]{2}",
+              end: "\\."
+            },
+            {
+              className: "meta",
+              begin: "@",
+              end: "\\W",
+              excludeEnd: true
+            }
+          ],
+          illegal: /([;<']|BEGIN)/
+        };
+      });
+
+      // from https://github.com/isagalaev/highlight.js/pull/1684/files
       hljs.registerLanguage("cypher", function(hljs) {
         // ...
 
